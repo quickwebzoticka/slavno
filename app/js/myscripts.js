a45bacf1e83db58ce929b4bc287499b2ce6c9524
Andrey Sokolow <quick.sokolow94@gmail.com>
2018-12-21 11:24:30 +0300

    finished adaptive poselok

--- app/js/myscripts.js
+++ app/js/myscripts.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 	$(window).resize(function(){
 		widthWindow  = $(window).width();
 	});
-	if (widthWindow <= 1024) {
+
 		var slider3 = $('.gallery-wrapper_1  .slider-block-inn').slick({
 			arrows:false,
 			slidesToShow: 2,
@@ -21,6 +21,12 @@ $(document).ready(function(){
 			draggable: false,
 			swipe: false,
 			responsive: [
+				{
+					breakpoint: 1026,
+					settings: {
+						slidesToShow: 2
+					}
+				},
 				{
 					breakpoint: 1025,
 					settings: {
@@ -44,6 +50,12 @@ $(document).ready(function(){
 			swipe: false,
 			responsive: [
 				{
+					breakpoint: 1026,
+					settings: {
+						slidesToShow: 2
+					}
+				},
+				{
 					breakpoint: 1025,
 					settings: {
 						slidesToShow: 2
@@ -57,52 +69,7 @@ $(document).ready(function(){
 				}
 			]
 		});
-	} else {
-		var slider3 = $('.gallery-wrapper_1  .slider-block-inn').slick({
-			arrows:false,
-			slidesToShow: 2,
-			slidesToScroll: 1,
-			infinite: false,
-			draggable: false,
-			swipe: false,
-			responsive: [
-				{
-					breakpoint: 1025,
-					settings: {
-						slidesToShow: 2
-					}
-				},
-				{
-					breakpoint: 480,
-					settings: {
-						slidesToShow: 1
-					}
-				}
-			]
-		});
-		var slider2 = $('.gallery-wrapper_2 .slider-block-inn').slick({
-			arrows:false,
-			slidesToShow: 2,
-			slidesToScroll: 1,
-			infinite: false,
-			draggable: false,
-			swipe: false,
-			responsive: [
-				{
-					breakpoint: 1025,
-					settings: {
-						slidesToShow: 2
-					}
-				},
-				{
-					breakpoint: 480,
-					settings: {
-						slidesToShow: 1
-					}
-				}
-			]
-		});
-	}
+
 	$(document).on('click', '.gallery-wrapper_1 .slider-block-prev', function (e) {
 		slider3.slick('slickPrev');
 		e.preventDefault();
